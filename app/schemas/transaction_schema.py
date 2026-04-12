54a84a3769a894b3c60f3e40de75eb8137a3419a
Mateus Torres <mmtorres230726@gmail.com>
2026-04-12 14:33:27 -0300

    creating a dedicated schema for transactions between accounts

--- app/schemas/transaction_schema.py
+++ app/schemas/transaction_schema.py
@@ -5,15 +5,22 @@ from app.models.transaction import TransactionType
 
 class TransactionBase(BaseModel):
     amount: Decimal = Field(gt=0, max_digits=10, decimal_places=2)
-    type: TransactionType
 
 class TransactionCreate(TransactionBase):
     user_id: int
     account_sequence_number: int
+    type: TransactionType
+
+class TransferCreate(TransactionBase):
+    """Schema dedicated for transfers between accounts."""
+    source_sequence_number: int
+    destination_user_id: int
+    destination_sequence_number: int
 
 class TransactionRead(TransactionBase):
     model_config = ConfigDict(from_attributes=True)
     id: int
-    user_id: int
+    account_user_id: int
     account_sequence_number: int
+    type: TransactionType
     timestamp: datetime
