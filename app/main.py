9067e00c519d780ada0eab093a5971ee439a5d90
Mateus Torres <mmtorres230726@gmail.com>
2026-04-12 14:16:34 -0300

    changing the name of the folder, to something better representative

--- app/main.py
+++ app/main.py
@@ -2,8 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.db.database import engine
 from app.models import Base
-from app.api.auth_router import router as auth_router
-
+from app.routers.auth_router import router as auth_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
